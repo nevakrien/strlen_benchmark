9963c483d76c715ff28571cfef36cf4c649174a9
neva <nevo.krien@gmail.com>
2025-03-25 20:17:09 +0200

    working version

--- main.c
+++ main.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define NUM_RUN 100000000
-#define STRING_LENGTH 100  // Length of each string
-#define NUM_STRINGS 100000  // Number of strings to store in the buffer
+#define NUM_RUN 100
+#define STRING_LENGTH 100
+#define NUM_STRINGS 1000000
 
-// Declare the function you're benchmarking
 int my_strlen(const char* s);
 
 int main() {
-    // Allocate a large buffer of strings packed together
-    char buffer[NUM_STRINGS][STRING_LENGTH];
+    // Allocate a large buffer of strings on the heap
+    char (*buffer)[STRING_LENGTH] = malloc(NUM_STRINGS * STRING_LENGTH);
+    if (!buffer) {
+        perror("malloc failed");
+        return 1;
+    }
 
     // Initialize the buffer with some data
     for (int i = 0; i < NUM_STRINGS; i++) {
         snprintf(buffer[i], STRING_LENGTH, "This is string number %d", i);
     }
 
-    // Benchmark loop: call my_strlen(NUM_RUN) times
+    // Benchmark loop
     for (int i = 0; i < NUM_RUN; i++) {
-        // Loop over the strings in the buffer and call my_strlen on each one
         for (int j = 0; j < NUM_STRINGS; j++) {
-            int len = my_strlen(buffer[j]);  // This is where you're benchmarking
-            (void)len; // To avoid unused variable warning
+            int len = my_strlen(buffer[j]);
+            (void)len;
         }
     }
 
+    free(buffer);
     return 0;
 }
